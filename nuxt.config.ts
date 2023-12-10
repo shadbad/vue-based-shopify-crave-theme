e33e86b93ae496bee06c830f3f0b2e88fab1f686
Sina Akbari Shadbad <shadbad@gmail.com>
2023-12-10 17:32:36 +0100

    feat: add husky

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,2 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({ devtools: { enabled: true } });
+export default defineNuxtConfig({ devtools: { enabled: true } })
